8a7c7e4c5445c30b85e90601427043e844731264
uchanchlani <utkarshc@tamu.edu>
2019-01-18 20:12:01 -0600

    part 1 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,81 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  res = 0
+  arr.each do |a|
+    res += a
+  end
+  return res
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  a = nil
+  b = nil
+  arr.each do |elem|
+    if a==nil
+      a=elem
+    elsif b==nil or b<elem
+      b=elem
+    end
+
+    if a!=nil and b!=nil and a<b
+      a,b=b,a
+    end
+  end
+
+  if a==nil
+    a=0
+  end
+  if b==nil
+    b=0
+  end
+  return a+b
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  # If there are less than 2 elems then we return false
+  if arr.length<2
+    return false
+  end
+
+  # The algo used runs in O(n log n),
+  # we sort the array,
+  # then for every element we search for the counter element in log n using binary search
+  # If found return true, else continue the search
+  # if no element found return false
+
+  # sort in reverse order
+  arr.sort! {|a, b| b <=> a}
+
+  # we iterate in reverse order
+  # Reason for reverse order iteration is that bsearch will return the first element's index which satisfies the condition
+  # in case when n is even and we have two elements with value n/2, n/2
+  # in this case because we search from backward, we encounter the later n/2 and bsearch will return the former n/2 and thus because we will get
+  # two different indices, we will return true.
+  # Had we done forward search, the condtion marked as THIS would have ignored the case that we discussed
+  arr.to_enum.with_index.reverse_each do |val, idx|
+    # break at this point (Why?)
+    if val > n/2
+      break
+    end
+
+    # search for other index (bsearch returns the first index which "MAY" satisfy the condition we want)
+    other_idx = arr.bsearch_index {|a| a <= (n-val)}
+
+    # if we didn't get the index or we get the same element's index which we are searching, this means we didn't got any element so we continue our search
+    # THIS
+    if other_idx==nil or other_idx==idx
+      next
+    end
+
+    # if the two values are found we break with returning true
+    if (val + arr[other_idx]) == n
+      return true
+    end
+  end
+
+  # else we return false
+  return false
 end
 
 # Part 2
